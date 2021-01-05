b6b498ca3d3bc2cb569873b7fb3e1cdfcad36b53
MubbashirAhmedKhan <mubbashirahmedkhan876@gmail.com>
2021-01-05 14:12:47 +0500

    assignment of javascript

---
+++ app.js
@@ -0,0 +1,197 @@
+// alert("Error! Please enter a password."); 
+
+// alert("Welcome to JS Land...\n Happy Coding");
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \n  Prevent this page from creating additional dialogs");
+
+// console.log("Hello..I can run JS through  my web browser's console")
+
+//  var username;
+//  var myName = "Mubbashir ahmed khan";
+//  alert(myName);
+
+// var message;
+// message = "HELLO WORLD";
+// alert(message);
+
+// var a = "John Doe";
+// var b = "15 years old";
+// var c = "Certified Mobile Application Development";
+// alert(a)
+// alert(b)
+// alert(c)
+
+// var a = "PIZZA"
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// var email = "mubbashirahmedkhan876@gmail.com"
+// alert("My email address is mubbashirahmedkhan876@gmail.com")
+
+// var book = "A smarter way to learn JavaScript."
+// alert("I am trying to learn from the Book A smarter \n way to learn JavaScript");
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// var age = 15;
+// alert("I am 15 years old");
+
+// alert("You have visited this site 14 times ");
+
+// var birthYear = 2003;
+// document.write("My birthYear is 2003 </br> Data type of my declared variable is number");
+
+// var name = "John Doe";
+// var product = "T-Shirts";
+// var quantity = 5;
+// document.write("<b>John Doe</b> ordered <b>5 T-shirt</b>(s) on XYZ Clothing store");
+
+// var variable1 = "Hello, World!";
+// var variable2 = "Testing...";
+// var variable3 = 42;
+
+// LEGAL VARIABLES:
+// var firstName;
+// var $mubbashir;
+// var at_gmail;
+// var go1time;
+// var _myName;
+
+// ILLEGAL NAMES:
+// var 1stName;
+// var at-gmail; 
+// var @gmail;
+// var go to;
+// var Place;
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("Variable names can only contain,numbers,$ and _.For example:$my_1stVariable </br>Variable must begin with a letter,$ or _.For example:$name, _name or name </br>Variable names are case sensitive </br>Variable names should not be JS keywords")
+
+// var a = 3;
+// var b = 5;
+// var c = a + b;
+// alert(c);
+// document.write("Sum of 3 and 5 is 8");
+
+// var a = 3;
+// var b = 5;
+// var c = a - b;
+// alert(c);
+// document.write("Subtraction of 3 and 5 is -2");
+
+// var a = 3;
+// var b = 5;
+// var c = a * b;
+// alert(c);
+// document.write("Multiplication of 3 and 5 is 15");
+
+// var a = 3;
+// var b = 5;
+// var c = a / b;
+// alert(c);
+// document.write("Division of 3 and 5 is 0.6");
+
+// var a = 3;
+// var b = 5;
+// var c = a % b;
+// alert(c);
+// document.write("Modulus of 3 and 5 is 3");
+
+// var a;
+// var b = 5;
+// var c = ++b;
+// alert(c);
+// var d = ++b + c;
+// alert(d);
+// var e = --d;
+// alert(e);
+// var f = e % 3;
+// alert(f);
+//  document.write("Value after variable declaration is undefined </br> Initial value:5 </br>Value after increment is:6 </br> Value after addition is:13 </br> Value after decrement is:12 </br>The remainder is:0 ");
+
+// var ticketPrice = 600;
+// var buyingTickets = 5;
+// var totalCost = ticketPrice * buyingTickets ;
+// document.write("Total Cost to buy 5 tickets to a movie is 3000PKR");
+
+// document.write("2 * 1 = 2 </br> 2 * 2 = 4 </br>2 * 3 = 6 </br>2 * 4 = 8  </br>2 * 5 = 10 </br>2 * 6 = 12 </br>2 * 7 = 14 </br>2 * 8 = 16 </br>2 * 9 = 18 </br>2 * 10 = 20 </br>")
+
+//  var cTemp = 25;
+//  var cToFahr = cTemp * 9 / 5 + 32;
+//  document.write(cToFahr);
+
+//  var fTemp = 70;
+//    var fToCel = (fTemp - 32) * 5 / 9;
+//    document.write(fToCel);
+
+//  var a = 650;
+//  var b = 3;
+//  var c = 100;
+//  var d = 7;
+//  var e = 100;
+//  var f = a*b+c*d+e
+ 
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("Price of item 1 is 650 </br> Quantity of item 1 is 3 </br>Price of item 2 is 100</br>Quantity of item 2 is 7 </br>Shipping Charges 100</br></br>Total cost of your order is ")
+// document.write(f);
+
+// var totalMarks = 1000;
+// var marksObtained = 968;
+// var percentage = marksObtained * 100 / totalMarks;
+// document.write("<h1>MarkSheet</h1> </br>Total Marks:1000 </br>Marks Obtained:968 </br>Percentage: ")
+// document.write(percentage)
+
+// var a = 50;
+// var b = a + 5;
+// var c = b * 10;
+// var d = c/2;
+// alert(d)
+
+// var cuurentYear = 2021;
+// var birthYear = 2003;
+// var age = cuurentYear % birthYear;
+//  document.write("<h1>Age Calculator</h1>")
+//  document.write("Current Year:")
+//  document.write(cuurentYear)
+//  document.write("Birth Year")
+//  document.write(birthYear)
+//  document.write("Your age is:")
+//  document.write(age)
+
+//  var radius = 20;
+//  var cicumference = 2*3.142;
+//  var x = radius * cicumference;
+ 
+//  var area = 3.142 * 40;
+//   document.write("<h1>The Geometrizer</h1> </br> Radius of a circle:20 </br>The circumference is:</br>The area is:</br>")
+//   document.write(area)
+  
+// var a = 2;
+// var b = 1;
+// var c = --a;
+// alert(c)
+// var d = --a - --b;
+// alert(d)
+// var e = --a - --b + ++b;
+// alert(e)
+// var f = --a - --b + ++b + b--;
+// alert(f)
+ 
+// var name = prompt("Enter your name: ");
+// alert("Your name is " + name)
+
+// var num = prompt("Please enter a table" , "your number is")
+// document.write(" 2 * 1 = 2 </br> 2 * 2 = 4 </br> 2 * 3 = 6 </br> 2 * 4 = 8 </br> 2 * 5 = 10 </br>2 * 6 = 12 </br> 2 * 7 = 14 </br> 2 * 8 = 16 </br> 2 * 9 = 18 </br>2 * 10 = 20 </br>")
+// document.write(" 5 * 1 =5 </br> 5 * 2 = 10 </br> 5 * 3 = 15 </br> 5 * 4 = 20</br> 5 * 5 = 25 </br>5 * 6 = 30 </br> 5 * 7 = 35 </br> 5 * 8 = 40 </br> 5 * 9 = 45 </br>5 * 10 = 50 </br>")
+
+// var city = prompt('Please enter your city');
+// if("karachi")
+// alert('Welcome to the city of lights');
+
+var gender = prompt("Please enter your gender")
+if (gender == "male") 
+{alert("Good Morning Sir")}
+else  (gender =="female")
+ {alert("Good Morning Ma’am")}
